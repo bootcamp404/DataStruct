653bbefe5e881da41bfa0bb81da30fc0b2ec1cf5
Santiago Giraldo Hincapie <santiagogiraldo.aflab125@gmail.com>
2025-03-12 12:04:46 +0100

    Empezando ha programar la api en spring boot

---
+++ BackEnd/api/src/test/java/com/agencia_local/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia_local.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
