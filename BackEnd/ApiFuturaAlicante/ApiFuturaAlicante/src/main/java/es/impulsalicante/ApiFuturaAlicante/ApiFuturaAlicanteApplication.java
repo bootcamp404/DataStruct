ac406f6561b021b184b9af4a7b0f5512b2f93504
Santiago Giraldo Hincapie <santiagogiraldo.aflab125@gmail.com>
2025-03-18 14:54:32 +0100

    Creando la api en version 21 de java

---
+++ BackEnd/ApiFuturaAlicante/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/ApiFuturaAlicanteApplication.java
@@ -0,0 +1,13 @@
+package es.impulsalicante.ApiFuturaAlicante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFuturaAlicanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFuturaAlicanteApplication.class, args);
+	}
+
+}
---
+++ BackEnd/ApiFuturaAlicante/ApiFuturaAlicante/src/test/java/es/impulsalicante/ApiFuturaAlicante/ApiFuturaAlicanteApplicationTests.java
@@ -0,0 +1,13 @@
+package es.impulsalicante.ApiFuturaAlicante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFuturaAlicanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
