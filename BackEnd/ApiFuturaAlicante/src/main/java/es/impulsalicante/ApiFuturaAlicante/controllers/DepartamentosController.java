0c18a55d5687c34627f8ade846cf429506f27d00
Leo <leo.suarzz05@gmal.com>
2025-03-28 10:36:07 +0100

    a

--- BackEnd/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/controllers/DepartamentosController.java
+++ BackEnd/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/controllers/DepartamentosController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import lombok.*;
+
 
 import java.net.URI;
 
--- BackEnd/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/models/Departamento.java
+++ BackEnd/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/models/Departamento.java
@@ -1,14 +1,18 @@
 package es.impulsalicante.ApiFuturaAlicante.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
-@Getter @RequiredArgsConstructor @AllArgsConstructor @ToString
+@Table(name = "departamento")
+@Getter @RequiredArgsConstructor @AllArgsConstructor @ToString @EqualsAndHashCode
 public class Departamento {
     @Id
     private final Integer id;
+    @Column
     @Setter
     private String nombre;
 
