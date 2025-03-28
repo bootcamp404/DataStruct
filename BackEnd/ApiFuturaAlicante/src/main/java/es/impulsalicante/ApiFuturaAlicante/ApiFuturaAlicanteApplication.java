6c1280ad7afd6c4b9e16e7b9cb271d1a41ea4a80
Leo <leo.suarzz05@gmal.com>
2025-03-28 11:13:04 +0100

    a

--- BackEnd/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/ApiFuturaAlicanteApplication.java
+++ BackEnd/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/ApiFuturaAlicanteApplication.java
@@ -3,9 +3,12 @@ package es.impulsalicante.ApiFuturaAlicante;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) // no se necesite BD para ejecutar el programa
+@SpringBootApplication
+@EnableJpaRepositories("es.impulsalicante.ApiFuturaAlicante.repository")
+//(exclude = {DataSourceAutoConfiguration.class }) // no se necesite BD para ejecutar el programa
 public class ApiFuturaAlicanteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFuturaAlicanteApplication.class, args);
