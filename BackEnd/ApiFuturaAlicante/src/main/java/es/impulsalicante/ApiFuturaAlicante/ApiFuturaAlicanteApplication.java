80318ae2e1456118f5747ee83baafef1ce46d9be
IvanGarcia125 <ivangarcia.aflab125@gmail.com>
2025-04-08 10:27:18 +0200

    Cambio en la API para que permita modificarla externamente

--- BackEnd/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/ApiFuturaAlicanteApplication.java
+++ BackEnd/ApiFuturaAlicante/src/main/java/es/impulsalicante/ApiFuturaAlicante/ApiFuturaAlicanteApplication.java
@@ -2,7 +2,10 @@ package es.impulsalicante.ApiFuturaAlicante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -10,6 +13,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ApiFuturaAlicanteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFuturaAlicanteApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*") // Permite todos los orígenes (en desarrollo)
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 
 }
