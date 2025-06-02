460eae8604587a7aa1a33f34b8261c7f9f156277
IvanGarcia125 <ivangarcia.aflab125@gmail.com>
2025-06-02 11:04:32 +0200

    login arreglado, junto al register

--- FrontEnd/ng-task-19-2/tailwind.config.js
+++ FrontEnd/ng-task-19-2/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   content: [ './src/**/*.{html,ts}' ],
   theme: {
     extend: {
-      // puedes extender colores o fuentes aquí si lo necesitas
+      colors: {
+        primary: {
+          DEFAULT: '#1176E1',
+          dark: '#0D5CA8',
+          light: '#4A9EFF'
+        },
+        secondary: {
+          DEFAULT: '#20B26E',
+          dark: '#1A8F5A',
+          light: '#4DC88A'
+        },
+      },
     },
   },
   plugins: [],
