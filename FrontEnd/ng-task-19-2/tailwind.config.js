3b5b4e521ae3b1171e76ce18e9f771e2b3e87298
IvanGarcia125 <ivangarcia.aflab125@gmail.com>
2025-06-02 13:50:55 +0200

    test

--- FrontEnd/ng-task-19-2/tailwind.config.js
+++ FrontEnd/ng-task-19-2/tailwind.config.js
@@ -4,18 +4,7 @@ module.exports = {
   content: [ './src/**/*.{html,ts}' ],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#1176E1',
-          dark: '#0D5CA8',
-          light: '#4A9EFF'
-        },
-        secondary: {
-          DEFAULT: '#20B26E',
-          dark: '#1A8F5A',
-          light: '#4DC88A'
-        },
-      },
+      // puedes extender colores o fuentes aquí si lo necesitas
     },
   },
   plugins: [],
